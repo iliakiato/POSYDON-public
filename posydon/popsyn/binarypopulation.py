55432426b1330bd238994bdd80ed52b000ce1ccf
Ilia Kiato <ilia.qato@northwestern.edu>
2025-09-29 17:53:53 -0500

    distribute indices acroos job array option = False when we're sampling kicks

--- posydon/popsyn/binarypopulation.py
+++ posydon/popsyn/binarypopulation.py
@@ -256,9 +256,19 @@ class BinaryPopulation:
             # do local MPI or cluster job array evolution
             indices = kw.get('indices',
                             list(range(self.number_of_binaries)))
-            indices_split = np.array_split(indices, self.size)
-            batch_indices = indices_split[self.rank]
-            mpi_tqdm_bool = True if (tqdm_bool and self.rank == 0) else False
+            
+            ##MODIFIED
+            if kw.get('distribute_indices_across_jobarray', True):
+                #default
+                
+                indices_split = np.array_split(indices, self.size)
+                batch_indices = indices_split[self.rank]
+                mpi_tqdm_bool = True if (tqdm_bool and self.rank == 0) else False
+                
+            else:
+                batch_indices = indices
+                mpi_tqdm_bool = tqdm_bool
+                
 
             params = {'indices':batch_indices,
                       'tqdm':mpi_tqdm_bool,
